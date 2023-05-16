0340570a8c17f5a49a69d65dd51c6fc3aca66245
Yousef Shafik <yousef.shafik@techsa.io>
2023-05-16 16:15:49 +0300

    [Development] Adds functions to calculate padding and apply it on image for same padding
    - Adds calculatePaddingValue Function
    
    - Changes both inputImage function and createImage to apply padding to

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -17,72 +17,9 @@ using namespace msclr::interop;
 void printKernel(double*** kernel, int width, int height);
 void freeKernel(double*** kernel, int width);
 void inputKernel(double*** kernel, int* kernelSize);
-
-int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of image in w & h
-{
-	int* input;
-
-
-	int OriginalImageWidth, OriginalImageHeight;
-
-	//*********************************************************Read Image and save it to local arrayss*************************	
-	//Read Image and save it to local arrayss
-
-	System::Drawing::Bitmap BM(imagePath);
-
-	OriginalImageWidth = BM.Width;
-	OriginalImageHeight = BM.Height;
-	*w = BM.Width;
-	*h = BM.Height;
-	int *Red = new int[BM.Height * BM.Width];
-	int *Green = new int[BM.Height * BM.Width];
-	int *Blue = new int[BM.Height * BM.Width];
-	input = new int[BM.Height*BM.Width];
-	for (int i = 0; i < BM.Height; i++)
-	{
-		for (int j = 0; j < BM.Width; j++)
-		{
-			System::Drawing::Color c = BM.GetPixel(j, i);
-
-			Red[i * BM.Width + j] = c.R;
-			Blue[i * BM.Width + j] = c.B;
-			Green[i * BM.Width + j] = c.G;
-
-			input[i*BM.Width + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
-
-		}
-
-	}
-	return input;
-}
-
-
-void createImage(int* image, int width, int height, int index)
-{
-	System::Drawing::Bitmap MyNewImage(width, height);
-
-
-	for (int i = 0; i < MyNewImage.Height; i++)
-	{
-		for (int j = 0; j < MyNewImage.Width; j++)
-		{
-			//i * OriginalImageWidth + j
-			if (image[i*width + j] < 0)
-			{
-				image[i*width + j] = 0;
-			}
-			if (image[i*width + j] > 255)
-			{
-				image[i*width + j] = 255;
-			}
-			System::Drawing::Color c = System::Drawing::Color::FromArgb(image[i*MyNewImage.Width + j], image[i*MyNewImage.Width + j], image[i*MyNewImage.Width + j]);
-			MyNewImage.SetPixel(j, i, c);
-		}
-	}
-	MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
-	cout << "result Image Saved " << index << endl;
-}
-
+int	calculatePaddingValue(int kernelSize);
+int* inputImage(int* w, int* h, int padding, System::String^ imagePath);
+void createImage(int* image, int width, int height, int index);
 
 int main()
 {
@@ -94,24 +31,24 @@ int main()
 	std::string img;
 	img = "..//Data//Input//test.png";
 
-	imagePath = marshal_as<System::String^>(img);
-	int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
-	
-	//start_s = clock();
-	
+	imagePath = marshal_as<System::String^>(img);	
 	int kernelSize = 0;
 	double** kernel;
 
 	inputKernel(&kernel, &kernelSize);
 	printKernel(&kernel,kernelSize,kernelSize);
-	freeKernel(&kernel, kernelSize);
+	int padding = calculatePaddingValue(kernelSize);
+	int* imageData = inputImage(&ImageWidth, &ImageHeight,padding, imagePath);
+	createImage(imageData, ImageWidth, ImageHeight, 1);
 
+	//start_s = clock();
 	//stop_s = clock();
 	//TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
 	//createImage(imageData, ImageWidth, ImageHeight, 1);
 	//cout << "time: " << TotalTime << endl;
 
-	//free(imageData);
+	freeKernel(&kernel, kernelSize);
+	free(imageData);
 	system("pause");
 	return 0;
 
@@ -121,7 +58,6 @@ void printKernel(double*** kernel, int width, int height) {
 	cout << "kernel is" << endl;
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
-		
 			cout << (*kernel)[i][j] <<" ";
 		}
 		cout << endl;
@@ -148,5 +84,70 @@ void inputKernel(double*** kernel, int* kernelSize) {
 			cin >> (*kernel)[i][j];
 		}
 	}
+}
 
+int calculatePaddingValue(int kernelSize) {
+	return (kernelSize - 1) / 2;
+}
+
+int* inputImage(int* w, int* h, int padding, System::String^ imagePath){
+	int* input;
+	int OriginalImageWidth, OriginalImageHeight;
+
+	System::Drawing::Bitmap BM(imagePath);
+
+	OriginalImageWidth = BM.Width;
+	OriginalImageHeight = BM.Height;
+	cout << OriginalImageWidth << " " << OriginalImageHeight<<endl;
+
+	*w = BM.Width + 2 * padding;
+	*h = BM.Height + 2 * padding;
+
+	int* Red = new int[(*h) * (*w)];
+	int* Green = new int[(*h) * (*w)];
+	int* Blue = new int[(*h) * (*w)];
+	input = new int[(*h) * (*w)];
+
+	for (int i = 0; i < *w; i++) { //2002 0 -> 2001
+		for (int j = 0; j < *h; j++) { //1958 0 ->1957
+
+			if (i < padding || j < padding || i > BM.Width - 1 + padding || j > BM.Height - 1 + padding) {
+				Red[i * (*h)+ j] = 0;
+				Blue[i * (*h) + j] = 0;
+				Green[i * (*h) + j] = 0;
+				input[i * (*h) + j] = 0; //gray scale value equals the average of RGB values
+			}
+			else {
+				System::Drawing::Color c = BM.GetPixel(i - padding, j - padding);
+				Red[i * (*h) + j] = c.R;
+				Blue[i * (*h) + j] = c.B;
+				Green[i * (*h) + j] = c.G;
+				input[i * (*h) + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
+			}
+		}
+	}
+
+	return input;
+}
+
+void createImage(int* image, int width, int height, int index){
+	System::Drawing::Bitmap MyNewImage(width, height);
+
+	for (int i = 0; i < MyNewImage.Width; i++) {
+		for (int j = 0; j < MyNewImage.Height; j++) {
+			if (image[i * height + j] < 0)
+			{
+				image[i * height + j] = 0;
+			}
+			if (image[i * height + j] > 255)
+			{
+				image[i * height + j] = 255;
+			}
+			System::Drawing::Color c = System::Drawing::Color::FromArgb(image[i * MyNewImage.Height + j], image[i * MyNewImage.Height + j], image[i * MyNewImage.Height + j]);
+			MyNewImage.SetPixel(i, j, c);
+		}
+	}
+
+	MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
+	cout << "result Image Saved " << index << endl;
 }
